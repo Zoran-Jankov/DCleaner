157a1d01a77fdba4ccf59fca8c5826926502046e
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-01 14:16:15 +0200

    Added GUI

--- File-Sorter.ps1
+++ File-Sorter.ps1
@@ -16,7 +16,7 @@
 .OUTPUTS
   Log file stored in "%APPDATA%\File-Sorter\File-Sorter-Log.log"
 .NOTES
-  Version:        0.1
+  Version:        0.8
   Author:         Zoran Jankov
   Creation Date:  30.06.2020.
   Purpose/Change: Initial script development
@@ -32,6 +32,119 @@ $documentExtensions = "*.DOC", "*.DOCX", "*.HTML", "*.HTM", "*.ODT", "*.PDF", "*
 $installerExtensions = "*.exe", "*.msi", "*.msm", "*.msp", "*.mst", "*.idt", "*.idt", "*.cub", "*.pcp"
 $pictureExtensions = "*.JPG", "*.PNG", "*.GIF", "*.WEBP", "*.TIFF", "*.SD", "*.RAW", "*.BMP", "*.HEIF", "*.INDD", "*.JPEG", "*.SVG", "*.AI", "*.EPS", "*.PDF"
 $videoExtensions = "*.WEBM", "*.MPG", "*.MP2", "*.MPEG", "*.MPE", "*.MPV", "*.OGG", "*.MP4", "*.M4P", "*.M4V", "*.AVI", "*.WMV", "*.MO", "*.QT", "*.FLV", "*.SWF", "*.AVCHD"
+
+#Create GUI
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$formFileSorter                  = New-Object system.Windows.Forms.Form
+$formFileSorter.ClientSize       = New-Object System.Drawing.Point(795,245)
+$formFileSorter.text             = "File Sorter"
+$formFileSorter.TopMost          = $true
+
+$lblDownloads                    = New-Object system.Windows.Forms.Label
+$lblDownloads.text               = "Downloads"
+$lblDownloads.AutoSize           = $true
+$lblDownloads.width              = 25
+$lblDownloads.height             = 10
+$lblDownloads.location           = New-Object System.Drawing.Point(20,20)
+$lblDownloads.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$txtDownloads                    = New-Object system.Windows.Forms.TextBox
+$txtDownloads.multiline          = $false
+$txtDownloads.width              = 600
+$txtDownloads.height             = 20
+$txtDownloads.location           = New-Object System.Drawing.Point(170,16)
+$txtDownloads.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$txtDownloads.ForeColor          = [System.Drawing.ColorTranslator]::FromHtml("#9b9b9b")
+
+$lblDocuments                    = New-Object system.Windows.Forms.Label
+$lblDocuments.text               = "Documents"
+$lblDocuments.AutoSize           = $true
+$lblDocuments.width              = 25
+$lblDocuments.height             = 10
+$lblDocuments.location           = New-Object System.Drawing.Point(20,50)
+$lblDocuments.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$txtDocuments                    = New-Object system.Windows.Forms.TextBox
+$txtDocuments.multiline          = $false
+$txtDocuments.width              = 600
+$txtDocuments.height             = 20
+$txtDocuments.location           = New-Object System.Drawing.Point(170,46)
+$txtDocuments.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$lblPictures                     = New-Object system.Windows.Forms.Label
+$lblPictures.text                = "Pictures"
+$lblPictures.AutoSize            = $true
+$lblPictures.width               = 25
+$lblPictures.height              = 10
+$lblPictures.location            = New-Object System.Drawing.Point(20,80)
+$lblPictures.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$txtPictures                     = New-Object system.Windows.Forms.TextBox
+$txtPictures.multiline           = $false
+$txtPictures.width               = 600
+$txtPictures.height              = 20
+$txtPictures.location            = New-Object System.Drawing.Point(170,76)
+$txtPictures.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$lblVideos                       = New-Object system.Windows.Forms.Label
+$lblVideos.text                  = "Videos"
+$lblVideos.AutoSize              = $true
+$lblVideos.width                 = 25
+$lblVideos.height                = 10
+$lblVideos.location              = New-Object System.Drawing.Point(20,110)
+$lblVideos.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$txtVideos                       = New-Object system.Windows.Forms.TextBox
+$txtVideos.multiline             = $false
+$txtVideos.width                 = 600
+$txtVideos.height                = 20
+$txtVideos.location              = New-Object System.Drawing.Point(170,107)
+$txtVideos.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$btnSortFiles                    = New-Object system.Windows.Forms.Button
+$btnSortFiles.text               = "Sort Files"
+$btnSortFiles.width              = 150
+$btnSortFiles.height             = 30
+$btnSortFiles.location           = New-Object System.Drawing.Point(325,200)
+$btnSortFiles.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10,[System.Drawing.FontStyle]([System.Drawing.FontStyle]::Bold))
+
+$lblProgramInstallers            = New-Object system.Windows.Forms.Label
+$lblProgramInstallers.text       = "Program Installers"
+$lblProgramInstallers.AutoSize   = $true
+$lblProgramInstallers.width      = 25
+$lblProgramInstallers.height     = 10
+$lblProgramInstallers.location   = New-Object System.Drawing.Point(20,140)
+$lblProgramInstallers.Font       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$TextBox1                        = New-Object system.Windows.Forms.TextBox
+$TextBox1.multiline              = $false
+$TextBox1.width                  = 0
+$TextBox1.height                 = 20
+$TextBox1.location               = New-Object System.Drawing.Point(144,140)
+$TextBox1.Font                   = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$txtProgramInstallers            = New-Object system.Windows.Forms.TextBox
+$txtProgramInstallers.multiline  = $false
+$txtProgramInstallers.width      = 600
+$txtProgramInstallers.height     = 20
+$txtProgramInstallers.location   = New-Object System.Drawing.Point(170,136)
+$txtProgramInstallers.Font       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$btnDefaultLocations             = New-Object system.Windows.Forms.Button
+$btnDefaultLocations.text        = "Default Locations"
+$btnDefaultLocations.width       = 150
+$btnDefaultLocations.height      = 30
+$btnDefaultLocations.location    = New-Object System.Drawing.Point(50,200)
+$btnDefaultLocations.Font        = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+$btnSaveLocations                = New-Object system.Windows.Forms.Button
+$btnSaveLocations.text           = "Save Locations"
+$btnSaveLocations.width          = 150
+$btnSaveLocations.height         = 30
+$btnSaveLocations.location       = New-Object System.Drawing.Point(590,200)
+$btnSaveLocations.Font           = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 
@@ -61,7 +174,13 @@ $logFile = Join-Path -Path $appPath -ChildPath $logName
 
 #Costum Folder Location File Info
 $costumFoldersName = "Costum-Folders.cvs"
-$costumFoldersFile = Join-Path -Path $appPath -ChildPath $costumFoldersName 
+$costumFoldersFile = Join-Path -Path $appPath -ChildPath $costumFoldersName
+
+#Load Locations Folders
+if(Test-Path costumFoldersFile -eq $True)
+{
+    
+}
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
@@ -118,6 +237,20 @@ Function Move-Files
     }
 }
 
+#Saves costum folder locations to local file
+Function Save-FolderSettings
+{
+    if((Test-Path $costumFoldersFile) -eq $False)
+    {
+        New-Item -Path $costumFoldersFile -ItemType File
+        Add-Content -Path $costumFoldersFile -Value '"FolderName","Path"'
+    }
+    else
+    {
+        Import-Csv $costumFoldersFile
+    }
+}
+
 #Starts files moving from source to user library folders
 Function Start-FileSorting
 {
@@ -134,7 +267,7 @@ Function Start-FileSorting
 
     End
     {
-        If($true)
+        If($True)
         {
             Log-Write -LogPath $logFile -LineValue "Completed Successfully."
             Log-Write -LogPath $logFile -LineValue "=============================================================================="
