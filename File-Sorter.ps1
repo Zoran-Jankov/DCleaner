5097e816f15e3e3dcce3b43341a94ee1f857a4b5
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-02 15:32:35 +0200

    Update File-Sorter.ps1

--- File-Sorter.ps1
+++ File-Sorter.ps1
@@ -28,10 +28,10 @@
 $ErrorActionPreference = "SilentlyContinue"
 
 #Set known extensions
-$documentExtensions = "*.DOC", "*.DOCX", "*.HTML", "*.HTM", "*.ODT", "*.PDF", "*.XLS", "*.XLSX", "*.ODS", "*.PPT", "*.PPTX", "*.TXT", "*.LOG"
-$installerExtensions = "*.exe", "*.msi", "*.msm", "*.msp", "*.mst", "*.idt", "*.idt", "*.cub", "*.pcp"
-$pictureExtensions = "*.JPG", "*.PNG", "*.GIF", "*.WEBP", "*.TIFF", "*.SD", "*.RAW", "*.BMP", "*.HEIF", "*.INDD", "*.JPEG", "*.SVG", "*.AI", "*.EPS", "*.PDF"
-$videoExtensions = "*.WEBM", "*.MPG", "*.MP2", "*.MPEG", "*.MPE", "*.MPV", "*.OGG", "*.MP4", "*.M4P", "*.M4V", "*.AVI", "*.WMV", "*.MO", "*.QT", "*.FLV", "*.SWF", "*.AVCHD"
+$global:documentExtensions = "*.DOC", "*.DOCX", "*.HTML", "*.HTM", "*.ODT", "*.PDF", "*.XLS", "*.XLSX", "*.ODS", "*.PPT", "*.PPTX", "*.TXT", "*.LOG"
+$global:installerExtensions = "*.exe", "*.msi", "*.msm", "*.msp", "*.mst", "*.idt", "*.idt", "*.cub", "*.pcp"
+$global:pictureExtensions = "*.JPG", "*.PNG", "*.GIF", "*.WEBP", "*.TIFF", "*.SD", "*.RAW", "*.BMP", "*.HEIF", "*.INDD", "*.JPEG", "*.SVG", "*.AI", "*.EPS", "*.PDF"
+$global:videoExtensions = "*.WEBM", "*.MPG", "*.MP2", "*.MPEG", "*.MPE", "*.MPV", "*.OGG", "*.MP4", "*.M4P", "*.M4V", "*.AVI", "*.WMV", "*.MO", "*.QT", "*.FLV", "*.SWF", "*.AVCHD"
 
 #Create GUI
 Add-Type -AssemblyName System.Windows.Forms
@@ -50,12 +50,12 @@ $lblDownloads.height             = 10
 $lblDownloads.location           = New-Object System.Drawing.Point(20,20)
 $lblDownloads.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$txtDownloads                    = New-Object system.Windows.Forms.TextBox
-$txtDownloads.multiline          = $false
-$txtDownloads.width              = 600
-$txtDownloads.height             = 20
-$txtDownloads.location           = New-Object System.Drawing.Point(170,16)
-$txtDownloads.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$global:txtDownloads                    = New-Object system.Windows.Forms.TextBox
+$global:txtDownloads.multiline          = $false
+$global:txtDownloads.width              = 600
+$global:txtDownloads.height             = 20
+$global:txtDownloads.location           = New-Object System.Drawing.Point(170,16)
+$global:txtDownloads.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $lblDocuments                    = New-Object system.Windows.Forms.Label
 $lblDocuments.text               = "Documents"
@@ -65,12 +65,12 @@ $lblDocuments.height             = 10
 $lblDocuments.location           = New-Object System.Drawing.Point(20,50)
 $lblDocuments.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$txtDocuments                    = New-Object system.Windows.Forms.TextBox
-$txtDocuments.multiline          = $false
-$txtDocuments.width              = 600
-$txtDocuments.height             = 20
-$txtDocuments.location           = New-Object System.Drawing.Point(170,46)
-$txtDocuments.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$global:txtDocuments                    = New-Object system.Windows.Forms.TextBox
+$global:txtDocuments.multiline          = $false
+$global:txtDocuments.width              = 600
+$global:txtDocuments.height             = 20
+$global:txtDocuments.location           = New-Object System.Drawing.Point(170,46)
+$global:txtDocuments.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $lblPictures                     = New-Object system.Windows.Forms.Label
 $lblPictures.text                = "Pictures"
@@ -80,12 +80,12 @@ $lblPictures.height              = 10
 $lblPictures.location            = New-Object System.Drawing.Point(20,80)
 $lblPictures.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$txtPictures                     = New-Object system.Windows.Forms.TextBox
-$txtPictures.multiline           = $false
-$txtPictures.width               = 600
-$txtPictures.height              = 20
-$txtPictures.location            = New-Object System.Drawing.Point(170,76)
-$txtPictures.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$global:txtPictures                     = New-Object system.Windows.Forms.TextBox
+$global:txtPictures.multiline           = $false
+$global:txtPictures.width               = 600
+$global:txtPictures.height              = 20
+$global:txtPictures.location            = New-Object System.Drawing.Point(170,76)
+$global:txtPictures.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $lblVideos                       = New-Object system.Windows.Forms.Label
 $lblVideos.text                  = "Videos"
@@ -95,12 +95,12 @@ $lblVideos.height                = 10
 $lblVideos.location              = New-Object System.Drawing.Point(20,110)
 $lblVideos.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$txtVideos                       = New-Object system.Windows.Forms.TextBox
-$txtVideos.multiline             = $false
-$txtVideos.width                 = 600
-$txtVideos.height                = 20
-$txtVideos.location              = New-Object System.Drawing.Point(170,107)
-$txtVideos.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$global:txtVideos                       = New-Object system.Windows.Forms.TextBox
+$global:txtVideos.multiline             = $false
+$global:txtVideos.width                 = 600
+$global:txtVideos.height                = 20
+$global:txtVideos.location              = New-Object System.Drawing.Point(170,107)
+$global:txtVideos.Font                  = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $btnSortFiles                    = New-Object system.Windows.Forms.Button
 $btnSortFiles.text               = "Sort Files"
@@ -118,12 +118,12 @@ $lblProgramInstallers.height     = 10
 $lblProgramInstallers.location   = New-Object System.Drawing.Point(20,140)
 $lblProgramInstallers.Font       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$txtProgramInstallers            = New-Object system.Windows.Forms.TextBox
-$txtProgramInstallers.multiline  = $false
-$txtProgramInstallers.width      = 600
-$txtProgramInstallers.height     = 20
-$txtProgramInstallers.location   = New-Object System.Drawing.Point(170,136)
-$txtProgramInstallers.Font       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+$global:txtProgramInstallers            = New-Object system.Windows.Forms.TextBox
+$global:txtProgramInstallers.multiline  = $false
+$global:txtProgramInstallers.width      = 600
+$global:txtProgramInstallers.height     = 20
+$global:txtProgramInstallers.location   = New-Object System.Drawing.Point(170,136)
+$global:txtProgramInstallers.Font       = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
 $btnDefaultLocations             = New-Object system.Windows.Forms.Button
 $btnDefaultLocations.text        = "Default Locations"
@@ -139,8 +139,9 @@ $btnSaveLocations.height         = 30
 $btnSaveLocations.location       = New-Object System.Drawing.Point(590,200)
 $btnSaveLocations.Font           = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
 
-$formFileSorter.controls.AddRange(@($lblDownloads,$txtDownloads,$lblDocuments,$txtDocuments,$lblPictures,$txtPictures,$lblVideos,
-$txtVideos,$btnSortFiles,$lblProgramInstallers,$TextBox1,$txtProgramInstallers,$btnDefaultLocations,$btnSaveLocations))
+$formFileSorter.controls.AddRange(@($lblDownloads,$global:txtDownloads,$lblDocuments,$global:txtDocuments,$lblPictures,
+$global:txtPictures,$lblVideos,$global:txtVideos,$btnSortFiles,$lblProgramInstallers,$global:txtProgramInstallers,
+$btnDefaultLocations,$btnSaveLocations))
 
 $btnSortFiles.Add_Click({ Start-FileSorting })
 $btnDefaultLocations.Add_Click({ Set-DefaultLocations })
@@ -148,26 +149,23 @@ $btnSaveLocations.Add_Click({ Save-FolderSettings })
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 
-#Timestamp Definition
-$timestamp = Get-Date -Format "dd.MM/yyyy HH:mm:ss"
-
 #Default Paths
-$defaultSourceFolder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
-$defaultPicturesFolder = [environment]::getfolderpath("mypictures")
-$defaultProgramInstallersFolder = "D:\Program Installers\"
-$defaultDocumentsFolder = [environment]::getfolderpath("mydocuments")
-$defaultVideosFolder = [environment]::getfolderpath("myvideos")
+$global:defaultSourceFolder = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
+$global:defaultPicturesFolder = [environment]::getfolderpath("mypictures")
+$global:defaultProgramInstallersFolder = "D:\Program Installers\"
+$global:defaultDocumentsFolder = [environment]::getfolderpath("mydocuments")
+$global:defaultVideosFolder = [environment]::getfolderpath("myvideos")
 
 #Aplication Folder Info
-$appPath = $env:APPDATA + "\File-Sorter"
+$global:appPath = $env:APPDATA + "\File-Sorter"
 
 #Log File Info
-$logName = "File-Sorter-Log.log"
-$logFile = Join-Path -Path $appPath -ChildPath $logName
+$global:logName = "File-Sorter-Log.log"
+$global:logFile = Join-Path -Path $global:appPath -ChildPath $global:logName
 
 #Costum Folder Location File Info
-$costumFoldersName = "Costum-Folders.cvs"
-$costumFoldersFile = Join-Path -Path $appPath -ChildPath $costumFoldersName
+$global:costumFoldersName = "Costum-Folders.cvs"
+$global:costumFoldersFile = Join-Path -Path $global:appPath -ChildPath $global:costumFoldersName
 
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 
@@ -191,7 +189,7 @@ Function Write-Log
 
     $logEntry = $timestamp + " - " + $Message 
 
-    Log-Write -LogPath $logFile -LineValue $logEntry
+    Log-Write -LogPath $global:logFile -LineValue $logEntry
 }
 
 #Moves files with defined extensions from source folder to defined destination folder
@@ -207,46 +205,17 @@ Function Move-Files
     {
         foreach($extension in $Extensions)
         {
-            $path = $sourceFolder + $extension
+            $path = $global:sourceFolder + $extension
 
-            $file = Get-ChildItem -Path $path
+            Get-ChildItem -Path $path | Move-Item -Destination $Destination
 
-            Move-Item -Path $file -Destination $Destination
-
-            $massage = $file.Name + " moved to " + $Destination
-
-            Write-Log -Message $massage
         }
     }
 
     Catch
     {
-        Log-Error -LogPath $logFile -ErrorDesc $_.Exception -ExitGracefully $True
+        Log-Error -LogPath $global:logFile -ErrorDesc $_.Exception -ExitGracefully $True
         Break
-    }
-}
-
-#Resets folder locations to default values
-function Set-DefaultLocations
-{
-    $txtDownloads.Text = $defaultSourceFolder
-    $txtPictures.Text = $defaultPicturesFolder
-    $txtProgramInstallers.Text = $defaultProgramInstallersFolder
-    $txtDocuments.Text = $defaultDocumentsFolder
-    $txtVideos.Text = $defaultVideosFolder
-}
-
-#Saves costum folder locations to local file
-Function Save-FolderSettings
-{
-    if((Test-Path $costumFoldersFile) -eq $False)
-    {
-        New-Item -Path $costumFoldersFile -ItemType File
-        Add-Content -Path $costumFoldersFile -Value '"FolderName";"Path"'
-    }
-    else
-    {
-        Import-Csv $costumFoldersFile
     }
 }
 
@@ -256,38 +225,55 @@ Function Start-FileSorting
     Write-Log -Message "File sorting started"
 
     #Get Locations
-    $sourceFolder = $txtDownloads.Text
-    $documentsFolder = $txtDocuments.Text
-    $picturesFolder = $txtPictures.Text
-    $videosFolder = $txtVideos.Text
-    $programInstallersFolder = $txtProgramInstallers.Text
+    $global:sourceFolder = $global:txtDownloads.Text
+    $documentsFolder = $global:txtDocuments.Text
+    $picturesFolder = $global:txtPictures.Text
+    $videosFolder = $global:txtVideos.Text
+    $programInstallersFolder = $global:txtProgramInstallers.Text
 
     #Moves files from source to user library folders 
-    Move-Files -Extensions $documentExtensions -Destination $documentsFolder
-    Move-Files -Extensions $pictureExtensions -Destination $picturesFolder
-    Move-Files -Extensions $videoExtensions -Destination $videosFolder
-    Move-Files -Extensions $installerExtensions -Destination $programInstallersFolder
+    Move-Files -Extensions $globaldocumentExtensions -Destination $documentsFolder
+    Move-Files -Extensions $globalpictureExtensions -Destination $picturesFolder
+    Move-Files -Extensions $globalvideoExtensions -Destination $videosFolder
+    Move-Files -Extensions $globalinstallerExtensions -Destination $programInstallersFolder
     
-    Log-Write -LogPath $logFile -LineValue "Completed Successfully."
-    Log-Write -LogPath $logFile -LineValue "=============================================================================="
+    Log-Write -LogPath $global:logFile -LineValue "Completed Successfully."
+    Log-Write -LogPath $global:logFile -LineValue "==============================================================================="
+}
 
+#Resets folder locations to default values
+Function Set-DefaultLocations
+{
+    $global:txtDownloads.Text = $global:defaultSourceFolder
+    $global:txtPictures.Text = $global:defaultPicturesFolder
+    $global:txtProgramInstallers.Text = $global:defaultProgramInstallersFolder
+    $global:txtDocuments.Text = $global:defaultDocumentsFolder
+    $global:txtVideos.Text = $global:defaultVideosFolder
+}
+
+#Saves costum folder locations to local file
+Function Save-FolderSettings
+{
+    if((Test-Path $global:costumFoldersFile) -eq $False)
+    {
+        New-Item -Path $global:costumFoldersFile -ItemType File
+        Add-Content -Path $global:costumFoldersFile -Value '"FolderName";"Path"'
+    }
+    else
+    {
+        Import-Csv $global:costumFoldersFile
+    }
 }
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 
 #Load Locations Folders
-if((Test-Path $costumFoldersFile) -eq $True)
+if((Test-Path $global:costumFoldersFile) -eq $True)
 {
     Set-DefaultLocations
 }
-else
-{
-    
-}
 
-New-ItemConditionalCreation -Item $appPath -Type Directory
-New-ItemConditionalCreation -Item $logFile -Type File
+New-ItemConditionalCreation -Item $global:appPath -Type Directory
+New-ItemConditionalCreation -Item $global:logFile -Type File
 
 [void]$formFileSorter.ShowDialog()
-
-Log-Finish -LogPath $logFile
